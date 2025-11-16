80976631c62cd65fe3c2eb43a7a45c2ab309a4ac
mathis2006titi <cocotte3euros@gmail.com>
2025-11-16 21:01:25 +0100

    Ajout de Gunicorn et Procfile pour Render + mise à jour requirements

--- app.py
+++ app.py
@@ -96,6 +96,6 @@ def download(filename):
 # ------------------------------
 # Lancement serveur
 # ------------------------------
-if __name__ == "__main__":
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
